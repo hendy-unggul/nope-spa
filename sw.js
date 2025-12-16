5fd76a245b82ada9a22cb75fba8b2c440dcd9ffc
NOPEfounderHD <nope.bukanguebanget@gmail.com>
2025-12-17 05:57:38 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,44 +1,24 @@
-// Service Worker untuk NOPE PWA
+// Service Worker untuk NOPE
 const CACHE_NAME = 'nope-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-192.png'
-];
 
-// Install Service Worker
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
-        return cache.addAll(urlsToCache);
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/manifest.json'
+        ]);
       })
   );
 });
 
-// Fetch assets
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // Return cached version or fetch new
         return response || fetch(event.request);
       })
-  );
-});
-
-// Update Service Worker
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
   );
 });
